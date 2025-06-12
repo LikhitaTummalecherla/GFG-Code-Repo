917a0d5e865afd5ee1e4a958d3620bcb0081277c
LikhitaTummalecherla <165761594+LikhitaTummalecherla@users.noreply.github.com>
2025-06-12 20:15:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K closest elements/k-closest-elements.java
@@ -0,0 +1,34 @@
+class Solution {
+    int[] printKClosest(int[] arr, int k, int x) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> {
+                int diffA = Math.abs(a - x);
+                int diffB = Math.abs(b - x);
+                if (diffA != diffB) {
+                    return diffB - diffA;
+                } else {
+                    return a - b; 
+                }
+            }
+        );
+
+        for (int num : arr) {
+            if (num != x) {
+                pq.add(num);
+            }
+            if (pq.size() > k) {
+                pq.poll(); 
+            }
+        }
+
+         int[] ans = new int[k];
+        int index = k - 1;
+        while (!pq.isEmpty()) {
+            ans[index--] = pq.poll();
+        }
+
+        return ans;
+        
+    }
+}
